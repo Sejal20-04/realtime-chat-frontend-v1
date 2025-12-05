0721d72b599880d43e8d2b1f4c90a46d12703502
Sejal20-04 <khobragadesejal90@gmail.com>
2025-12-05 23:32:50 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://realtime-chat-backend-jrp0.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password })
